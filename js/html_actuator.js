f43730fe64a6c78dbd17d96fc50bd6f1ddd79d32
Rin Davis <Rin@Rins-MacBook-Air.local>
2016-04-20 22:15:53 -0500

    first commit

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -124,7 +124,7 @@ HTMLActuator.prototype.updateBestScore = function (bestScore) {
 
 HTMLActuator.prototype.message = function (won) {
   var type    = won ? "game-won" : "game-over";
-  var message = won ? "Good work." : "Filthy peasant.";
+  var message = won ? "Just like me." : "Filthy peasant.";
 
   this.messageContainer.classList.add(type);
   this.messageContainer.getElementsByTagName("p")[0].textContent = message;
